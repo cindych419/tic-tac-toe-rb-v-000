48d6fc8a154dcf35cefbd812b4d67aae86fb1462
cindych419@gmail.com <cindych419@gmail.com>
2018-04-25 08:08:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,10 +73,11 @@ def turn(board)
 def play(board)
   while !over?(board)
     turn(board)
+  end
+  
   if won?(board)
     puts "Congratulations #{winner(board)}"
   elsif draw?(board)
     puts "Cat's Game!"
   end
-end
 end
